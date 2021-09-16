96fcbd56260424307925637588b3b89784278057
Erik Oh <eoh555@gmail.com>
2021-09-16 16:09:30 -0700

    Implement DeFiService component without style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import DeFiServices from './DeFiServices';
 import { hot } from 'react-hot-loader';
+import Theme from '../Theme';
 
 function App() {
-
   return (
-    <div className="app">
-      {/* <USDCBalance /> */}
-      {/* <ProgressDaysWidget /> */}
-      {/* <DeFiServices /> */}
-    </div>
+    <Theme>
+      <div>
+        {/* <USDCBalance /> */}
+        {/* <ProgressDaysWidget /> */}
+        <DeFiServices />
+      </div>
+    </Theme>
   );
 }
 
